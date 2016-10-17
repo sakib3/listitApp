4c9b3642bcf782290cc7e713aa5065e5ae7b7e83
sakib3 <sakib3@gmail.com>
2016-10-17 21:13:34 +0200

    login page modified and contains more fields and nav

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -5,13 +5,22 @@ import { Component } from '@angular/core';
 })
 export class LoginPage {
   usercreds: {email: string, password: string};
+  logInAs: string;
   constructor() {
     this.usercreds = {
         email: '',
         password: ''
     }
+    this.logInAs = "employee";
   }
   login(usercreds) {
     console.log('login req', usercreds);
   }
+  signUp(){
+
+  }
+  forgotPassWord(){
+
+  }
+
 }
