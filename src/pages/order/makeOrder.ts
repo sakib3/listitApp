d876516ffc0a82b5c8a8179255b4e3b0421f0807
sakib3 <sakib3@gmail.com>
2016-10-22 22:59:02 +0200

    product Model is added and nighmare is resolved! by chnaging product model extension js to ts :D

--- src/pages/order/makeOrder.ts
+++ src/pages/order/makeOrder.ts
@@ -1,18 +1,21 @@
 import { OnInit, Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Company } from './companyModel';
+import { Product } from './productModel';
 import { CompanyService } from './companyService';
+import { ProductService } from './productService';
 
 @Component({
   selector: 'page-makeOrder',
   templateUrl: 'makeOrder.html',
-  providers: [CompanyService]
+  providers: [CompanyService, ProductService]
 })
 export class MakeOrderPage implements OnInit{
   companies: Company[];
+  products: Product[];
   selectedCompany: Company;
 
-  constructor(public navCtrl: NavController, private companyService: CompanyService) {
+  constructor(public navCtrl: NavController, private companyService: CompanyService, private productService: ProductService) {
     //this.getCompanies();
     this.selectedCompany = new Company();
   }
@@ -20,9 +23,15 @@ export class MakeOrderPage implements OnInit{
     //this.companies = this.companyService.getCompanies();
     this.companyService.getCompanies().then(companies => this.companies = companies);
   }
+  getProducts(): void {
+    //this.companies = this.companyService.getCompanies();
+    this.productService.getProducts().then(products => this.products = products);
+  }
   ngOnInit(): void {
     console.info('On init!!!!')
     this.getCompanies();
+    this.getProducts();
+    console.log(this.products);
   }
   companySelected(id) : void{
     if(id!==undefined) this.selectedCompany = this.companies.find(c=>c.id==id);
---
+++ src/pages/order/productService.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Product } from './productModel';
+import { PRODUCTS } from './mock-products';
+
+
+@Injectable()
+export class ProductService {
+  getProducts(): Promise<Product[]> {
+    return Promise.resolve(PRODUCTS);
+  }
+  getProductsSlowly(): Promise<Product[]> {
+    return new Promise<Product[]>(resolve =>
+      setTimeout(resolve, 2000)) // delay 2 seconds
+      .then(() => this.getProducts());
+  }
+}
