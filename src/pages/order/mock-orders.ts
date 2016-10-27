af0bc6f6abcb97be4679b95d43449b06ff283316
sakib3 <sakib3@gmail.com>
2016-10-27 22:27:51 +0200

    order moc

--- src/pages/order/mock-orders.ts
+++ src/pages/order/mock-orders.ts
@@ -5,6 +5,6 @@ export const ORDER: Order =
 
 export const ORDERS: Order[] =
 [
-  {id : '1', company: 'A', employee: 'B', products: [{p_id:1,name:'p1',quantity:1}, {p_id:2,name:'p2',quantity:10},{p_id:3,name:'p3',quantity:5}], order_date: '2016-10-22', delivery_date: '2016-10-23', status: true},
-  {id : '2', company: 'C', employee: 'D', products: [], order_date: '2016-10-23', delivery_date: '2016-10-23', status: true}
+  {id : '1', company: 'Tieto AB', employee: 'B', products: [{p_id:1,name:'Alkaline',quantity:1}, {p_id:2,name:'Medel',quantity:10},{p_id:3,name:'Hand gloves',quantity:5}], order_date: '2016-10-22', delivery_date: '2016-10-23', status: true},
+  {id : '2', company: 'Ericson AB', employee: 'D', products: [{p_id:1,name:'Alkaline',quantity:1}], order_date: '2016-10-23', delivery_date: '2016-10-23', status: true}
 ];
