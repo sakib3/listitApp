46d89a7ffcd84c246f68f17d675ff4de8d98de18
sakib3 <sakib3@gmail.com>
2016-11-05 17:47:03 +0100

    home page modified

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Nav } from 'ionic-angular';
+import { LoginPage } from '../../pages/login/login';
+import { SignUpPage } from '../../pages/signUp/signUp';
 @Component({
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor() {
+  constructor(private navCtrl:Nav) {
 
+  }
+  navigateLogin(){
+    this.navCtrl.push(LoginPage);
+  }
+  navigateSignup(){
+    this.navCtrl.push(SignUpPage);
   }
 }
